26440a307cfe6379f4dfb140c1ac30a1db860a80
cjchallis <christopher.challis@gmail.com>
2015-04-28 10:36:41 -0400

    First passing version of bob, works with jasmine

--- bob/bob.js
+++ bob/bob.js
@@ -1,14 +1,11 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    input = input.trim();
+    if (input === '') {return "Fine. Be that way!"}
+    if (input === input.toUpperCase() && input.match(/[a-z]/i)) {
+        return "Whoa, chill out!"
+    }
+    if (input.slice(-1) === '?') {return "Sure."}
+    return "Whatever."
 };
-
-module.exports = Bob;
