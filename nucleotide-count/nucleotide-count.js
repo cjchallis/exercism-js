4ebce152088535e8b869ab4a7a1457a481c95541
cjchallis <christopher.challis@gmail.com>
2015-05-12 16:56:34 +0000

    Update nucleotide count

--- nucleotide-count/nucleotide-count.js
+++ nucleotide-count/nucleotide-count.js
@@ -1,13 +1,11 @@
 var dna = function(strand) {
-    if (strand === undefined) {
-        strand = ''
-    }
+    strand = strand || ''
     var counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0};
     for (i = 0, len = strand.length; i < len; i++) {
         if (!(strand[i] in counts)) {
             throw new Error                 
         }
-        counts[strand[i]] += 1;
+        counts[strand[i]]++;
     }
             
     return {
